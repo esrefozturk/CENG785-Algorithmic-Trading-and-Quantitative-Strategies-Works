1e79be2dd0d426c66619062a39f66f14764c1bbd
Esref Ozturk <esrefozturk93@gmail.com>
2017-01-01 14:21:22 +0300

    neural network code added

---
+++ hw2/StockMarket/src/org/neuroph/contrib/samples/stockmarket/Main.java
@@ -0,0 +1,108 @@
+/***
+ * The Example is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Neuroph. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neuroph.contrib.samples.stockmarket;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.learning.SupervisedTrainingElement;
+import org.neuroph.core.learning.TrainingElement;
+import org.neuroph.core.learning.TrainingSet;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.learning.LMS;
+
+/**
+ *
+ * @author Dr.V.Steinhauer
+ */
+public class Main {
+    
+    public  static void calc(String filename,int maxIterations ) throws FileNotFoundException, IOException{
+                ArrayList<Double> ar = new ArrayList<Double>();
+        FileInputStream fis = new FileInputStream(filename);
+        BufferedReader dis = new BufferedReader(new InputStreamReader(fis));
+        String s;
+        while ((s = dis.readLine()) != null) {
+            ar.add(Double.parseDouble(s));
+        }
+        NeuralNetwork neuralNet = new MultiLayerPerceptron(5, 9, 1);
+        ((LMS) neuralNet.getLearningRule()).setMaxError(0.001);//0-1
+        ((LMS) neuralNet.getLearningRule()).setLearningRate(0.7);//0-1
+        ((LMS) neuralNet.getLearningRule()).setMaxIterations(maxIterations);//0-1
+        TrainingSet trainingSet = new TrainingSet();
+        for(int i=0;i<ar.size()-20-5-1;i++){
+          
+            trainingSet.addElement(new SupervisedTrainingElement(new double[]{ar.get(i)/1000,ar.get(i+1)/1000,ar.get(i+2)/1000,ar.get(i+3)/1000,ar.get(i+4)/1000 },
+                    new double[]{ar.get(i+5)/1000}));
+            
+        
+        }
+        
+       
+        
+        neuralNet.learnInSameThread(trainingSet);
+        
+        TrainingSet testSet = new TrainingSet();
+        for(int i=ar.size()-20;i<ar.size()-5;i++){
+            testSet.addElement(new TrainingElement(new double[]{ar.get(i)/1000,ar.get(i+1)/1000,ar.get(i+2)/1000,ar.get(i+3)/1000,ar.get(i+4)/1000 }));
+        }
+        int i=ar.size()-20;
+        for (TrainingElement testElement : testSet.trainingElements()) {
+            neuralNet.setInput(testElement.getInput());
+            neuralNet.calculate();
+            Vector<Double> networkOutput = neuralNet.getOutput();
+            System.out.print("Input: " + testElement.getInput());
+            System.out.println(" Output: " + networkOutput);
+            //System.out.println(networkOutput.get(0)-ar.get(i+4)/1000  );
+            i++;
+        }
+
+    }
+    
+
+    public static void main(String[] args)  {
+        
+        try {
+            calc("../averages/BBRY.csv",1000);
+            calc("../averages/BHP.csv",1000);
+            calc("../averages/DE.csv",1000);
+            calc("../averages/FE.csv",1000);
+            calc("../averages/GOOG.csv",1000);
+            calc("../averages/GS.csv",1000);
+            calc("../averages/JNJ.csv",1000);
+            calc("../averages/KO.csv",1000);
+            calc("../averages/WMT.csv",1000);
+            calc("../averages/XOM.csv",1000);
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+
+        System.exit(0);
+    }
+}
--- hw2/a.py
+++ hw2/a.py
@@ -1,24 +1,24 @@
 from os import listdir
 from statistics import stdev, mean
+from scipy.stats import linregress
+from sklearn import svm, metrics
+import numpy as np
+from numpy import cov
 
 STOCKS_DIR = 'stocks'
 INDICES_DIR = 'indices'
 
-def abs(x):
-	return x if x>0 else -(x)
-
-def COV(x, y):
-	mx = mean(x)
-	my = mean(y)
-	n = len(x)
-	s = 0
-	for i in range(n):
-		s += abs(x[i]-mx)*abs(y[i]-my)
-	return s/n
 
 class Entity:
 
-	def __init__(self, type, file):
+	def print_averages(self):
+		with open('averages/%s.csv'%self.name, 'w') as f:
+			f.write( '\n'.join( [ str(i) for i in self.averages[::-1] ] ) + '\n' )
+
+
+	def __init__(self, type=None, file=None):
+		if file is None:
+			return
 		self.type = type
 		self.name = file
 		self.dates = []
@@ -53,17 +53,62 @@ class Entity:
 		self.stdev = stdev( self.returns[:-1] )
 		self.sharpe_ratio = self.average_return/self.stdev
 
+
 	def calc_alpha_beta(self, indice):
 		#print self.name,indice.name,COV(self.returns[:-1],indice.returns[:-1]),(indice.stdev**2)
-		self.betas[indice.name] = COV(self.returns[:-1],indice.returns[:-1])/(indice.stdev**2)
-		self.alphas[indice.name] = self.average_return - self.betas[indice.name]*indice.average_return
+		self.betas[indice.name], self.alphas[indice.name], _, _, _ = linregress(indice.returns[:-1],self.returns[:-1])
+
+	def build_portfolio(self, stocks, indices):
+		self.returns = []
+		self.betas = {}
+		self.alphas = {}
+		self.name = 'portfolio'
+
+		asdas = 0
+		for i in stocks:
+			for j in stocks:
+				asdas +=cov(i.returns[:-1],j.returns[:-1])[0][1]
+		self.stdev = asdas**0.5
+
+		print self.stdev
+
+		'''
+		for i in range(len(stocks[0].returns)):
+			total = 0
+			for s in stocks:
+				total += s.returns[i]
+			self.returns.append(total/len(stocks))
+
+
+
+
+		self.average_return = mean(self.returns[:-1])
+		self.stdev = stdev(self.returns[:-1])
+		for indice in indices:
+			self.betas[indice.name], self.alphas[indice.name], _, _, _ = linregress(indice.returns[:-1],self.returns[:-1])
+		'''
+
+
+def print_portfolio(filename, stock):
+	indices = stock.alphas.keys()
+	alphas = [ i.split('-')[1].strip()+'-alpha' for i in indices]
+	betas = [ i.split('-')[1].strip()+'-beta' for i in indices]
+	header = ['Portfolio','Average Return','Risk'] + alphas + betas
+	with open(filename,'w') as f:
+		f.write(','.join(header)+'\n')
+		lst = [ stock.name,str(stock.average_return),str(stock.stdev) ]
+		for indice in indices:
+			lst.append( str(stock.alphas[indice]) )
+		for indice in indices:
+			lst.append( str(stock.betas[indice]) )
+		f.write(','.join(lst)+'\n')
+
 
 def print_csv(filename, stocks):
 	indices = stocks[0].alphas.keys()
 	alphas = [ i.split('-')[1].strip()+'-alpha' for i in indices]
 	betas = [ i.split('-')[1].strip()+'-beta' for i in indices]
 	header = ['Stock','Average Return','Risk'] + alphas + betas + ['Sharpe Ratio']
-	stocks = sorted(stocks,key= lambda stock:-stock.sharpe_ratio)
 	with open(filename,'w') as f:
 		f.write(','.join(header)+'\n')
 		for stock in stocks:
@@ -78,6 +123,25 @@ def print_csv(filename, stocks):
 
 
 
+
+def svm_it(stock, WINDOW_SIZE=4, RATIO=0.8):
+	values = stock.averages[::-1]
+	m = max(values)
+	values = [ int((i/m)*100) for i in values ]
+	asd = []
+	qwe = []
+	for i in range(len(values)-WINDOW_SIZE-1):
+		asd.append(values[i:i+WINDOW_SIZE])
+		qwe.append(values[i+WINDOW_SIZE])
+	SIZE = len(asd)
+	S = int(SIZE*RATIO)
+	model = svm.SVC()
+	model.fit(asd[:S],qwe[:S])
+	predicted = model.predict(asd[S:])
+	expected = qwe[S:]
+	print metrics.confusion_matrix(expected,predicted)
+	print metrics.classification_report(expected,predicted)
+
 def main():
 
 	stocks = [ i.split('.')[0] for i in listdir(STOCKS_DIR) ]
@@ -87,11 +151,28 @@ def main():
 	indices = [ Entity('indice',i) for i in indices ]
 
 	for stock in stocks:
+		stock.print_averages()
 		for indice in indices:
 				stock.calc_alpha_beta(indice)
 
+	stocks = sorted(stocks,key= lambda stock:-stock.sharpe_ratio)
+
+	#for stock in stocks:
+	#	svm_it(stock)
+
 	print_csv("result.csv",stocks)
 
+	portfolio_stocks = stocks[:5]
+
+	print "Stock Counts:"
+	for stock in portfolio_stocks:
+		stock.count = int(200000/stock.averages[0])
+
+		print '\t',stock.name,':',stock.count,stock.average_return
+
+	portfolio = Entity()
+	portfolio.build_portfolio(portfolio_stocks,indices)
+	#print_portfolio("portfolio.csv",portfolio)
 
 
 
